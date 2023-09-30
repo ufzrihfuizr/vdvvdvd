b37264ce97f6920692086df29f1de22dd61d6c24
ufzrihfuizr <144798872+ufzrihfuizr@users.noreply.github.com>
2023-09-30 23:57:23 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Banner from 'Banner.css'
+// import Cart from './Cart'
+import ShoppingList from 'ShoppingList.css'
+
+function App() {
+	return (
+		<div>
+			<Banner />
+			{/* <Cart /> */}
+			<ShoppingList />
+		</div>
+	)
+}
+
+export default App
---
+++ ShoppingList.js
@@ -0,0 +1,26 @@
+import { plantList } from 'plantList.js'
+function ShoppingList() {
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
+
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul>
+				{plantList.map((plant) => (
+					<li key={plant.id}>{plant.name}</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+
+export default ShoppingList
